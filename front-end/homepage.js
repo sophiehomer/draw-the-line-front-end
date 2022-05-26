dc911295afc70f00548fd08d17eb1dba1fe9a74a
sophie homer <sophiehomer94@gmail.com>
2022-05-25 20:28:54 -0700

    styles

--- front-end/homepage.js
+++ front-end/homepage.js
@@ -1,5 +1,9 @@
 const logo = document.querySelectorAll('#logo path');
-
+const line = document.querySelectorAll('#line path')
 for(let i = 0; i <logo.length; i++) {
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+// for(let i = 0; i <line.length; i++) {
+//     console.log(`Line ${i} is ${logo[i].getTotalLength()}`);
+// }
